a5ed62ff6eadc25059c69b61d8a7f1b36781ba3a
Sam231221 <rasengankid999@gmail.com>
2022-05-29 14:37:08 +0545

    Build version added

---
+++ build/precache-manifest.16bc738c01343cba69727c7c052fdf43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a026f5131db5d413dc8a5594e6e49c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4ff4329e3bad9e6205b",
+    "url": "/static/css/main.9b097d89.chunk.css"
+  },
+  {
+    "revision": "23c452ea0768deda5202",
+    "url": "/static/js/2.2ad51991.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2ad51991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ff4329e3bad9e6205b",
+    "url": "/static/js/main.abd4196f.chunk.js"
+  },
+  {
+    "revision": "42236b3c5a0e9054fb96",
+    "url": "/static/js/runtime-main.0c82e416.js"
+  }
+]);
